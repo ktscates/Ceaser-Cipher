b8a5790b13cc8d4a29f0e0df1d925969b2f148dd
ktscates <cates2506@gmail.com>
2019-09-07 13:51:42 +0200

    Add the code for decryption

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,2 +1,25 @@
 public class Decrypt {
+
+    public static StringBuffer decryption(String cipherText, int key){
+
+        //Decrypt the text
+        StringBuffer decrypt = new StringBuffer();
+        for (int j = 0; j < cipherText.length(); j++){
+
+            //Check if text is upper case
+            if (Character.isUpperCase(cipherText.charAt(j))){
+                char d = (char) (((int)cipherText.charAt(j) + key -65) % 26 - 65);
+                decrypt.append(d);
+
+                //Check if text is lower case
+            } else {
+                char d = (char) (((int)cipherText.charAt(j) + key -97) % 26 - 97);
+                decrypt.append(d);
+
+            }
+        }
+
+        return decrypt;
+
+    }
 }
