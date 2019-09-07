7ef6e4649879ad6923172f5eabd160d33dd82740
ktscates <cates2506@gmail.com>
2019-09-07 13:18:53 +0200

    Minor changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,9 +8,9 @@ public class App {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a word: ");
         String plainText = input.nextLine();
-        System.out.println("The plain text is: ");
+        System.out.println("The plain text is: " + plainText);
         int key = 3;
-        System.out.println("Text  : " + plainText);
+//        System.out.println("Text  : " + );
         System.out.println("Shift : " + key);
         System.out.println("Cipher: " + Encrypt.encryption(plainText, key));
 
