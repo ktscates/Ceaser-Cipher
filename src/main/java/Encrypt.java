7dd350e538d15cbf8a4940a7ada89528ac606353
ktscates <cates2506@gmail.com>
2019-09-07 12:19:15 +0200

    Declare the alphabet and find the character in the alphabet

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,3 +1,16 @@
 public class Encrypt {
 
+    public String Encrypt(String plainText, int key){
+
+        //Declare the alphabet
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        //Find character in the alphabet
+        StringBuilder encrypt = new StringBuilder(plainText);
+        for(int i = 0; i < encrypt.length; i++){
+            char plainChar = encrypt.charAt(i);
+            int plainIndex = alphabet.indexOf(plainChar);
+        }
+    }
+
 }
