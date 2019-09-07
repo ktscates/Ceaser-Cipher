91484317323a832eb7996df8494811db1b54705b
ktscates <cates2506@gmail.com>
2019-09-07 14:26:17 +0200

    Add a code to enable the user to choose from different option

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,40 +1,70 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args){
 
-        //Input the plain text
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a word: ");
-        String plainText = input.nextLine();
-        System.out.println("The plain text is: " + plainText);
+        boolean theProgramIsRunning = true;
 
-        //Input the key or shift
-        Scanner inputKey = new Scanner(System.in);
-        System.out.println("Enter a Shift: ");
-        int key = Integer.parseInt((inputKey.nextLine()));
-        System.out.println("The shift is: " + key);
+        while (theProgramIsRunning){
 
-        //Output Ciphered text
-        System.out.println("The Cipher text is: " + Encrypt.encryption(plainText, key));
+            //Enable the user to choose an option
+            Scanner option = new Scanner(System.in);
+            System.out.println("Welcome to the Caeser Cipher Application. Choose one of the following options: Encrypt, Decrypt, or Exit");
 
-        //Input the cipher text
-        Scanner cipherInput = new Scanner(System.in);
-        System.out.println("Enter a word: ");
-        String cipherText = cipherInput.nextLine();
-        System.out.println("The plain text is: " + cipherText);
+            try{
 
-        //Input the key or shift
-        Scanner inputShift = new Scanner(System.in);
-        System.out.println("Enter a Shift: ");
-        int shift = Integer.parseInt((inputShift.nextLine()));
-        System.out.println("The shift is: " + shift);
+                String choice = option.nextLine();
 
-        //Output Ciphered text
-        System.out.println("The Plain text is: " + Decrypt.decryption(cipherText, shift));
+                if (choice.equals("Encrypt")){
 
+                    //Input the plain text
+                    Scanner input = new Scanner(System.in);
+                    System.out.println("Enter a word: ");
+                    String plainText = input.nextLine();
+                    System.out.println("The plain text is: " + plainText);
 
+                    //Input the key or shift
+                    Scanner inputKey = new Scanner(System.in);
+                    System.out.println("Enter a Shift: ");
+                    int key = Integer.parseInt((inputKey.nextLine()));
+                    System.out.println("The shift is: " + key);
+
+                    //Output Ciphered text
+                    System.out.println("The Cipher text is: " + Encrypt.encryption(plainText, key));
+                }
+                else if (choice.equals("Decrypt")){
+
+                    //Input the cipher text
+                    Scanner cipherInput = new Scanner(System.in);
+                    System.out.println("Enter a word: ");
+                    String cipherText = cipherInput.nextLine();
+                    System.out.println("The plain text is: " + cipherText);
+
+                    //Input the key or shift
+                    Scanner inputShift = new Scanner(System.in);
+                    System.out.println("Enter a Shift: ");
+                    int shift = Integer.parseInt((inputShift.nextLine()));
+                    System.out.println("The shift is: " + shift);
+
+                    //Output Ciphered text
+                    System.out.println("The Plain text is: " + Decrypt.decryption(cipherText, shift));
+                }
+                else if (choice.equals("Exit")){
+
+                    System.out.println("Goodbye, Thank you!");
+                    theProgramIsRunning = false;
+                }
+                else {
+
+                    System.out.println("We are sorry, this option does not exit. Please try again later!");
+                }
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
 
