98d49d24ac19e702eb797ffa9e270b8319d5e8d3
ktscates <cates2506@gmail.com>
2019-09-07 14:01:38 +0200

    Add front end interface and minus the shift

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class App {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
+
         //Input the plain text
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a word: ");
@@ -18,6 +18,22 @@ public class App {
 
         //Output Ciphered text
         System.out.println("The Cipher text is: " + Encrypt.encryption(plainText, key));
+
+        //Input the cipher text
+        Scanner cipherInput = new Scanner(System.in);
+        System.out.println("Enter a word: ");
+        String cipherText = cipherInput.nextLine();
+        System.out.println("The plain text is: " + cipherText);
+
+        //Input the key or shift
+        Scanner inputShift = new Scanner(System.in);
+        System.out.println("Enter a Shift: ");
+        int shift = Integer.parseInt((inputShift.nextLine()));
+        System.out.println("The shift is: " + shift);
+
+        //Output Ciphered text
+        System.out.println("The Plain text is: " + Decrypt.decryption(cipherText, shift));
+
 
     }
 }
--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,6 +1,6 @@
 public class Decrypt {
 
-    public static StringBuffer decryption(String cipherText, int key){
+    public static StringBuffer decryption(String cipherText, int shift){
 
         //Decrypt the text
         StringBuffer decrypt = new StringBuffer();
@@ -8,12 +8,12 @@ public class Decrypt {
 
             //Check if text is upper case
             if (Character.isUpperCase(cipherText.charAt(j))){
-                char d = (char) (((int)cipherText.charAt(j) + key -65) % 26 - 65);
+                char d = (char) (((int)cipherText.charAt(j) - shift -65) % 26 + 65);
                 decrypt.append(d);
 
                 //Check if text is lower case
             } else {
-                char d = (char) (((int)cipherText.charAt(j) + key -97) % 26 - 97);
+                char d = (char) (((int)cipherText.charAt(j) - shift -97) % 26 + 97);
                 decrypt.append(d);
 
             }
