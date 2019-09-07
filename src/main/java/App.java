2026a75ebb86671ff8421265652e576b746e3d3a
ktscates <cates2506@gmail.com>
2019-09-07 13:28:38 +0200

    Change the code for the user to input the shift key

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,15 +4,20 @@ public class App {
 
     public static void main(String[] args)
     {
-        //The code execution
+        //Input the plain text
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a word: ");
         String plainText = input.nextLine();
         System.out.println("The plain text is: " + plainText);
-        int key = 3;
-//        System.out.println("Text  : " + );
-        System.out.println("Shift : " + key);
-        System.out.println("Cipher: " + Encrypt.encryption(plainText, key));
+
+        //Input the key or shift
+        Scanner inputKey = new Scanner(System.in);
+        System.out.println("Enter a Shift: ");
+        int key = Integer.parseInt((inputKey.nextLine()));
+        System.out.println("The shift is: " + key);
+
+        //Output Ciphered text
+        System.out.println("The Cipher text is: " + Encrypt.encryption(plainText, key));
 
     }
 }
