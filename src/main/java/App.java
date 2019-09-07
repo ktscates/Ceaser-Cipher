ed86c7dba6c3db205973a480cda9a5270eb8be76
ktscates <cates2506@gmail.com>
2019-09-07 14:34:41 +0200

    Remove try-catch error

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class App {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
 
         boolean theProgramIsRunning = true;
 
@@ -13,56 +13,52 @@ public class App {
             Scanner option = new Scanner(System.in);
             System.out.println("Welcome to the Caeser Cipher Application. Choose one of the following options: Encrypt, Decrypt, or Exit");
 
-            try{
 
-                String choice = option.nextLine();
 
-                if (choice.equals("Encrypt")){
+            String choice = option.nextLine();
 
-                    //Input the plain text
-                    Scanner input = new Scanner(System.in);
-                    System.out.println("Enter a word: ");
-                    String plainText = input.nextLine();
-                    System.out.println("The plain text is: " + plainText);
+            if (choice.equals("Encrypt")){
 
-                    //Input the key or shift
-                    Scanner inputKey = new Scanner(System.in);
-                    System.out.println("Enter a Shift: ");
-                    int key = Integer.parseInt((inputKey.nextLine()));
-                    System.out.println("The shift is: " + key);
+                //Input the plain text
+                Scanner input = new Scanner(System.in);
+                System.out.println("Enter a word: ");
+                String plainText = input.nextLine();
+                System.out.println("The plain text is: " + plainText);
 
-                    //Output Ciphered text
-                    System.out.println("The Cipher text is: " + Encrypt.encryption(plainText, key));
-                }
-                else if (choice.equals("Decrypt")){
+                //Input the key or shift
+                Scanner inputKey = new Scanner(System.in);
+                System.out.println("Enter a Shift: ");
+                int key = Integer.parseInt((inputKey.nextLine()));
+                System.out.println("The shift is: " + key);
 
-                    //Input the cipher text
-                    Scanner cipherInput = new Scanner(System.in);
-                    System.out.println("Enter a word: ");
-                    String cipherText = cipherInput.nextLine();
-                    System.out.println("The plain text is: " + cipherText);
-
-                    //Input the key or shift
-                    Scanner inputShift = new Scanner(System.in);
-                    System.out.println("Enter a Shift: ");
-                    int shift = Integer.parseInt((inputShift.nextLine()));
-                    System.out.println("The shift is: " + shift);
-
-                    //Output Ciphered text
-                    System.out.println("The Plain text is: " + Decrypt.decryption(cipherText, shift));
-                }
-                else if (choice.equals("Exit")){
-
-                    System.out.println("Goodbye, Thank you!");
-                    theProgramIsRunning = false;
-                }
-                else {
-
-                    System.out.println("We are sorry, this option does not exit. Please try again later!");
-                }
+                //Output Ciphered text
+                System.out.println("The Cipher text is: " + Encrypt.encryption(plainText, key));
             }
-            catch (IOException e){
-                e.printStackTrace();
+            else if (choice.equals("Decrypt")){
+
+                //Input the cipher text
+                Scanner cipherInput = new Scanner(System.in);
+                System.out.println("Enter a word: ");
+                String cipherText = cipherInput.nextLine();
+                System.out.println("The plain text is: " + cipherText);
+
+                //Input the key or shift
+                Scanner inputShift = new Scanner(System.in);
+                System.out.println("Enter a Shift: ");
+                int shift = Integer.parseInt((inputShift.nextLine()));
+                System.out.println("The shift is: " + shift);
+
+                //Output Ciphered text
+                System.out.println("The Plain text is: " + Decrypt.decryption(cipherText, shift));
+            }
+            else if (choice.equals("Exit")){
+
+                System.out.println("Goodbye, Thank you!");
+                theProgramIsRunning = false;
+            }
+            else {
+
+                System.out.println("We are sorry, this option does not exit. Please try again later!");
             }
         }
     }
