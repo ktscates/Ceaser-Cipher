cbfe282672d5aaba6139febd71d1d9a0b3c36beb
ktscates <cates2506@gmail.com>
2019-09-09 11:01:18 +0200

    Add encrypt test

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,3 +1,13 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class EncryptTest {
+
+    @Test
+    public void testEncrypt_testWord_String(){
+        Encrypt testingEncrypt = new Encrypt();
+        assertEquals(true, testingEncrypt instanceof Encrypt);
+    }
 
 }
