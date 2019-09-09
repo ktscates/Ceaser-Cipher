91640541f04b5eb933d6debc1b6500c5c7868482
ktscates <cates2506@gmail.com>
2019-09-09 11:04:38 +0200

    Add decrypt test

--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -1,5 +1,13 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class DecryptTest {
+
+    @Test
+    public void testDecrypt_testWord_String(){
+        Decrypt testingDecrypt = new Decrypt();
+        assertEquals(true, testingDecrypt instanceof Decrypt);
+    }
 
 }
